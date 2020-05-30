e17b90826eb7eaf1ebfa4d89406c4b235464235a
Cherlas <vm@li.cm>
2020-05-30 23:17:07 +0800

    first commit

---
+++ src/main/java/cn/istarxc/tools/InterfaceGeneratorToolsApplication.java
@@ -0,0 +1,13 @@
+package cn.istarxc.tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterfaceGeneratorToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterfaceGeneratorToolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/istarxc/tools/InterfaceGeneratorToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.istarxc.tools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterfaceGeneratorToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
